fc00b158878110a231ff98a2429f9307dc87577d
Brandon Aaronson <45774261+brandonaaronson@users.noreply.github.com>
2020-09-20 14:15:32 -0700

    Geek City U.S.A.

--- deploy.js
+++ deploy.js
@@ -87,7 +87,7 @@ function sshConnect() {
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
       host: '54.242.149.188',
       username: 'ubuntu',
-      privateKey: 'MEAN-Stack.pem'
+      privateKey: 'MEAN-Stack.ppk'
     })
     .then(function() {
       console.log('SSH Connection established.');
